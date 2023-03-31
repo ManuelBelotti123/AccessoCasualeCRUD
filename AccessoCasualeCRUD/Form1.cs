b0936caca64d8c166a9bd13eff244a6f51116b4d
Belotti.20181 <Belotti.20181@LABI2-13PC3>
2023-03-31 13:19:45 +0200

    Aggiungere i file di progetto.

---
+++ AccessoCasualeCRUD/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccessoCasualeCRUD
+{
+    public partial class Form1 : Form
+    {
+
+        public struct prodotto
+        {
+            public string nome;
+            public int prezzo;
+            public int quantita;
+            public int c;
+        }
+
+        prodotto p;
+        string nomefile;
+        int lunghezzaRecord;
+        char sp;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //inizializzazioni
+            nomefile = "prodotti.txt";
+            lunghezzaRecord = 50;
+            sp = ';';
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
