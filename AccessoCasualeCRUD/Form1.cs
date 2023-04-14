cf0d22fc9b374c21fea15fb07ef2f573dc220ae8
Belotti.20181 <Belotti.20181@LABI2-13PC3>
2023-04-14 13:05:11 +0200

    Funzione Ricompatta completata

--- AccessoCasualeCRUD/Form1.cs
+++ AccessoCasualeCRUD/Form1.cs
@@ -86,9 +86,11 @@ namespace AccessoCasualeCRUD
         public void AggProd(string riga, string nomefile)
         {
             var oStream = new FileStream(nomefile, FileMode.Append, FileAccess.Write, FileShare.Read);
-            StreamWriter sw = new StreamWriter(oStream);
-            sw.WriteLine(riga);
-            sw.Close();
+            BinaryWriter writer = new BinaryWriter(oStream);
+            char[] linea = riga.ToCharArray();
+            writer.Write(linea);
+            writer.Close();
+            oStream.Close();
         }
 
         public prodotto RecOrCancLog(string prodottostringa, string sp, int c)
@@ -199,21 +201,33 @@ namespace AccessoCasualeCRUD
 
         public void Ricompatta(string nomefile, string sp, int l)
         {
+            prodotto p;
+            string line;
             byte[] br;
             var file = new FileStream(nomefile, FileMode.Open, FileAccess.ReadWrite);
+            var appoggio = new FileStream("appoggio.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
             BinaryReader reader = new BinaryReader(file);
-            BinaryWriter writer = new BinaryWriter(file);
-            file.Seek(l, SeekOrigin.Begin);
-            while (file.Position < file.Length - l)
+            BinaryWriter writer = new BinaryWriter(appoggio);
+            file.Seek(0, SeekOrigin.Begin);
+            appoggio.SetLength(0);
+            while (file.Position < file.Length)
             {
                 br = reader.ReadBytes(l);
-                file.Seek(-l*2, SeekOrigin.Current);
-                writer.Write(br);
-                file.Seek(l, SeekOrigin.Current);
+                line = Encoding.ASCII.GetString(br, 0, br.Length);
+                string[] div = line.Split(sp[0]);
+                string c = div[3];
+                if (c[0] != '0')
+                {
+                    writer.Write(br);
+                }
             }
+            file.SetLength(0);
+            appoggio.Position = 0;
+            appoggio.CopyTo(file);
             reader.Close();
             writer.Close();
             file.Close();
+            appoggio.Close();
         }
 
         public void Visualizza(string nomefile, string sp, int l)
